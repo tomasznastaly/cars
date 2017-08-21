a1a4a2c10f1b95d1e96ae8ee4bedfd89c5c5e8d3
tomasznastaly <nastalytomasz@gmail.com>
2017-08-21 21:07:11 +0200

    adding subscription and ngOnDestroy

--- src/app/cars/total-cost/income-tax/income-tax.component.ts
+++ src/app/cars/total-cost/income-tax/income-tax.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {CostSharedService} from "../../cost-shared.service";
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'cs-income-tax',
   templateUrl: './income-tax.component.html'
 })
-export class IncomeTaxComponent implements OnInit {
+export class IncomeTaxComponent implements OnInit, OnDestroy {
   private incomeTax : number = 18;
   income : number;
+  costSubscription : Subscription;
 
   constructor(private costSharedService : CostSharedService) {}
 
   ngOnInit() {
-    this.costSharedService.totalCostSource$.subscribe((cost) => {
+    this.costSubscription = this.costSharedService.totalCostSource$.subscribe((cost) => {
       this.income = cost * this.incomeTax / 100;
     });
+  }
+
+  ngOnDestroy() {
+    if (this.costSubscription) {
+      this.costSubscription.unsubscribe();
+    }
   }
 }
