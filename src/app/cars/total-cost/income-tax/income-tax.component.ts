7412bce88efecb0da407568980161b7f8b909c20
tomasznastaly <nastalytomasz@gmail.com>
2017-08-22 23:59:46 +0200

    fixing

--- src/app/cars/total-cost/income-tax/income-tax.component.ts
+++ src/app/cars/total-cost/income-tax/income-tax.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import {CostSharedService} from "../../cost-shared.service";
-import {Subscription} from 'rxjs/Subscription';
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
   selector: 'cs-income-tax',
