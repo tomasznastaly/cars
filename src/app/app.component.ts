c622c8edf778b1de27ce6452824c8179663dfab3
tomasznastaly <nastalytomasz@gmail.com>
2017-09-04 21:14:08 +0200

    spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationCancel, NavigationEnd, NavigationError } from '@angular/router';
 import {LayoutService} from "./shared-module/services/layout.service";
 
 @Component({
@@ -8,12 +9,28 @@ import {LayoutService} from "./shared-module/services/layout.service";
 })
 export class AppComponent {
   isSidebarVisible : boolean = false;
+  isLoading : boolean = false;
 
-  constructor(private layoutService : LayoutService) {}
+  constructor(private layoutService : LayoutService, private router : Router) {}
 
   ngOnInit() {
     this.layoutService.sidebarSource$.subscribe((isVisible) => {
       this.isSidebarVisible = isVisible;
     });
+
+    this.router.events.subscribe((routerEvent : Event) => {
+      console.log('routerEvent', routerEvent);
+      this.checkRouterEvent(routerEvent)
+    });
+  }
+
+  private checkRouterEvent(routerEvent : Event) {
+    if (routerEvent instanceof NavigationStart) {
+      this.isLoading = true;
+    } else if (routerEvent instanceof NavigationEnd
+      || routerEvent instanceof NavigationCancel
+      || routerEvent instanceof NavigationError) {
+      this.isLoading = false;
+    }
   }
 }
