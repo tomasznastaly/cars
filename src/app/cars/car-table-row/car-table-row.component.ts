c95ac5031e596946a7d4d00869873765624fd60b
tomasznastaly <nastalytomasz@gmail.com>
2017-09-12 21:12:55 +0200

    host binding

--- src/app/cars/car-table-row/car-table-row.component.ts
+++ src/app/cars/car-table-row/car-table-row.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, HostBinding, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import {Car} from "../models/car";
 
 @Component({
   selector: '[cs-car-table-row]',
   templateUrl: './car-table-row.component.html'
 })
-export class CarTableRowComponent {
+export class CarTableRowComponent implements OnInit {
   @Input() car : Car;
   @Output() removedCar = new EventEmitter();
+  @HostBinding('class.after-deadline') deadline : boolean = false;
+
+  ngOnInit() {
+    this.deadline = new Date(this.car.deadline) < new Date();
+  }
 
   removeCar(car, event) {
     event.stopPropagation();
