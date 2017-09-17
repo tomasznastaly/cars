dc8557cf064a8573398d3a7e63a166b7c30df6d7
tomasznastaly <nastalytomasz@gmail.com>
2017-09-17 17:04:51 +0200

    mark for check

--- src/app/shared-module/timer/timer.component.ts
+++ src/app/shared-module/timer/timer.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'cs-timer',
   templateUrl: './timer.component.html',
-  styleUrls: ['./timer.component.less']
+  styleUrls: ['./timer.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TimerComponent implements OnInit {
   time : Date = null;
 
+  constructor(private changeDetectorRef : ChangeDetectorRef) {}
+
   ngOnInit() {
     setInterval(() => {
       this.time = new Date();
+      this.changeDetectorRef.detectChanges();
     }, 1500)
   }
 }
